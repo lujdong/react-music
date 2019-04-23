be5a31a35ec2bff625201c458a44a72fc6fdfc2c
芦见东 <jiandong.lu@fanfor.com>
2019-04-23 18:10:31 +0800

    update and set routing

---
+++ src/components/ranking/Ranking.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './ranking.styl'
+
+class Ranking extends React.Component {
+    render() {
+        return (
+            <div className="music-ranking">
+                ranking
+            </div>
+        )
+    }
+}
+
+export default Ranking
---
+++ src/components/search/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './search.styl'
+
+class Search extends React.Component {
+    render() {
+        return (
+            <div className="music-search">
+                search
+            </div>
+        )
+    }
+}
+
+export default Search
